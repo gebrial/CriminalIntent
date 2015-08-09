50c7744de08b418b09b06253ff3220eb99becf06
gebrial <gebrial@live.ca>
2015-08-09 16:18:11 -0700

    Add DatePicker to AlertDialog.

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by sanjaya on 09/08/15.
@@ -12,7 +14,11 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
+        View view = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(view)
                 .setTitle(R.string.data_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
