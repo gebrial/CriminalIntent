875a257456e089685cb88ccfb3eec75db340da08
gebrial <gebrial@live.ca>
2015-08-08 17:12:18 -0700

    Add new main activity, CrimeListActivity.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
-
+/**
+ * Created by sanjaya on 08/08/15.
+ */
+public class CrimeListActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sanjaya on 08/08/15.
+ */
+public class CrimeListFragment extends Fragment {
+}
