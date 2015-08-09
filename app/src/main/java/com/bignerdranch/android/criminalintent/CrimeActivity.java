b346d90499b5c9fc50513a2ecda4b501ce89fd14
gebrial <gebrial@live.ca>
2015-08-08 21:54:42 -0700

    Use different information transfer method (to encapsulate CrimeFragment class better).

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -8,11 +8,11 @@ import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
-    public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return CrimeFragment.newInstance((UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID));
     }
 
     public static Intent newIntent(Context packageContext, UUID crimeId){
