f1125396e8b38a91ed5cc65fe0bd8295a7d19ff5
gebrial <gebrial@live.ca>
2015-08-09 19:32:06 -0700

    Fix time resetting to zero after picking new date. (cherry picked from commit 5c65f08)

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -13,7 +13,6 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 /**
  * Created by sanjaya on 09/08/15.
@@ -25,14 +24,16 @@ public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
 
     private DatePicker mDatePicker;
+    private Date mDate;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         View view = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
 
+        mDate = (Date) getArguments().getSerializable(ARG_DATE);
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime((Date) getArguments().getSerializable(ARG_DATE));
+        calendar.setTime(mDate);
 
         mDatePicker = (DatePicker) view.findViewById(R.id.dialog_date_date_picker);
         mDatePicker.init(calendar.get(Calendar.YEAR),
@@ -46,10 +47,13 @@ public class DatePickerFragment extends DialogFragment {
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Date date = new GregorianCalendar(mDatePicker.getYear(),
-                                                          mDatePicker.getMonth(),
-                                                          mDatePicker.getDayOfMonth()).getTime();
-                        sendResult(Activity.RESULT_OK, date);
+                        Calendar calendar2 = Calendar.getInstance();
+                        calendar2.setTime(mDate);
+                        calendar2.set(Calendar.YEAR, mDatePicker.getYear());
+                        calendar2.set(Calendar.MONTH, mDatePicker.getMonth());
+                        calendar2.set(Calendar.DAY_OF_MONTH, mDatePicker.getDayOfMonth());
+
+                        sendResult(Activity.RESULT_OK, calendar2.getTime());
                     }
                 })
                 .create();
