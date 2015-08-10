6f5ee2beba63215ee79c01e14c8a03411a2043ae
gebrial <gebrial@live.ca>
2015-08-10 16:01:51 -0700

    Create DatePickerFragment, instead of Dialog.

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -2,13 +2,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
@@ -24,11 +25,13 @@ public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
 
     private DatePicker mDatePicker;
+    private Button mConfirmButton;
     private Date mDate;
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
-        View view = LayoutInflater.from(getActivity())
+        /*View view = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
 
         mDate = (Date) getArguments().getSerializable(ARG_DATE);
@@ -56,7 +59,44 @@ public class DatePickerFragment extends DialogFragment {
                         sendResult(Activity.RESULT_OK, calendar2.getTime());
                     }
                 })
-                .create();
+                .create();*/
+
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.dialog_date, container, false);
+
+        mDate = (Date) getArguments().getSerializable(ARG_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+
+        mDatePicker = (DatePicker) view.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH),
+                        null);
+
+        mConfirmButton = (Button) view.findViewById(R.id.date_picker_button);
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar2 = Calendar.getInstance();
+                calendar2.setTime(mDate);
+                calendar2.set(Calendar.YEAR, mDatePicker.getYear());
+                calendar2.set(Calendar.MONTH, mDatePicker.getMonth());
+                calendar2.set(Calendar.DAY_OF_MONTH, mDatePicker.getDayOfMonth());
+
+                sendResult(Activity.RESULT_OK, calendar2.getTime());
+                dismiss();
+            }
+        });
+
+        return view;
     }
 
     private void sendResult(int resultCode, Date date){
