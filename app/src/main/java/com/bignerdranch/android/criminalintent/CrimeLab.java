5f7f36fd6544b08ab7a686a7d7f718f5be1eb5fc
gebrial <gebrial@live.ca>
2015-08-12 13:12:27 -0700

    Add database for list of crimes and gut CrimeLab.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +14,8 @@ import java.util.UUID;
  */
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null)
@@ -21,22 +25,18 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
-        mCrimes = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
     }
 
     public List<Crime> getCrimes(){
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id){
-        for(Crime crime : mCrimes){
-            if(crime.getId().equals(id))
-                return crime;
-        }
         return null;
     }
 
-    public void addCrime(Crime c){
-        mCrimes.add(c);
-    }
+    public void addCrime(Crime c){}
 }
