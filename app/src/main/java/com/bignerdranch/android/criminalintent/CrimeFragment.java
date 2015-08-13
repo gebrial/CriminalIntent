b9db9ebb433cf7ba31d3db93a6f677a00a54c37e
gebrial <gebrial@live.ca>
2015-08-13 15:21:44 -0700

    Show chooser on every "Send Report"

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -111,7 +111,7 @@ public class CrimeFragment extends Fragment {
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
                 intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
-                startActivity(intent);
+                startActivity(Intent.createChooser(intent, getString(R.string.send_report)));
             }
         });
 
